c999540507360a5f43a15008851f755e95353823
Mina Her <minacle@live.com>
2020-09-18 06:18:58 +0900

    Update KoreanigaTests.swift

--- Tests/KoreanigaTests/KoreanigaTests.swift
+++ Tests/KoreanigaTests/KoreanigaTests.swift
@@ -28,10 +28,11 @@ final class KoreanigaTests: XCTestCase {
     func testUDHR1() {
         let human = "인간"
         let dignity = "존엄"
+        let innate = "천부적"
         let reason = "이성"
         let conscience = "양심"
         let spirit = "정신"
-        XCTAssertEqual("모든 \(human.는) 태어날 때부터 자유로우며 그 \(dignity.과) 권리에 있어 동등하다. \(human.는) 천부적으로 \(reason.과) \(conscience.를) 부여받았으며 서로 형제애의 \(spirit.로) 행동하여야 한다.",
+        XCTAssertEqual("모든 \(human.는) 태어날 때부터 자유로우며 그 \(dignity.과) 권리에 있어 동등하다. \(human.는) \(innate.로) \(reason.와) \(conscience.를) 부여받았으며 서로 형제애의 \(spirit.로) 행동하여야 한다.",
                        "모든 인간은 태어날 때부터 자유로우며 그 존엄과 권리에 있어 동등하다. 인간은 천부적으로 이성과 양심을 부여받았으며 서로 형제애의 정신으로 행동하여야 한다.")
     }
 }
